5b4a9304980d48a80bee916f4d4f43ebb32516ba
helwyza <helwyza@gmail.com>
2024-10-03 01:58:28 +0700

    modif

--- backend/index.js
+++ backend/index.js
@@ -26,24 +26,12 @@ app.use(cors(corsOptions));
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send(`${process.env.SECRET_KEY}`);
+  return res.status(201).json({
+    message: "Account created successfully.",
+    success: true,
+  });
 });
 
-app.get("/token", (req, res) => {
-  const user = { _id: "123456" }; // Replace with actual user data
-  const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
-    expiresIn: "1h",
-  });
-  res.cookie("token", token, {
-    httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
-    maxAge: 3600000, // Cookie will expire in 1 hour
-  });
-  res.status(200).json({
-    success: true,
-    message: "Token generated successfully",
-    user: user, // Return user data if needed
-  });
-});
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
