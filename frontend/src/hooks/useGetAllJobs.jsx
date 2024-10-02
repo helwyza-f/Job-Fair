41fce8511f96844edf446bcc2be1fa53c30693c5
helwyza <helwyza@gmail.com>
2024-10-03 00:05:01 +0700

    modify hooks

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -1,25 +1,28 @@
-import { setAllJobs } from '@/redux/jobSlice'
-import { JOB_API_END_POINT } from '@/utils/constant'
-import axios from 'axios'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { setAllJobs } from "@/redux/jobSlice";
+import { JOB_API_END_POINT } from "@/utils/constant";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const useGetAllJobs = () => {
-    const dispatch = useDispatch();
-    const {searchedQuery} = useSelector(store=>store.job);
-    useEffect(()=>{
-        const fetchAllJobs = async () => {
-            try {
-                const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,{withCredentials:true});
-                if(res.data.success){
-                    dispatch(setAllJobs(res.data.jobs));
-                }
-            } catch (error) {
-                console.log(error);
-            }
+  const dispatch = useDispatch();
+  const { searchedQuery } = useSelector((store) => store.job);
+  useEffect(() => {
+    const fetchAllJobs = async () => {
+      try {
+        const res = await axios.get(
+          `${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,
+          { withCredentials: true }
+        );
+        if (res.data.success) {
+          dispatch(setAllJobs(res.data.jobs));
         }
-        fetchAllJobs();
-    },[])
-}
+      } catch (error) {
+        console.log(`errornya: ${error}`);
+      }
+    };
+    fetchAllJobs();
+  }, []);
+};
 
-export default useGetAllJobs
+export default useGetAllJobs;
