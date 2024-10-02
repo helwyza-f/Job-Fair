eb2f58366a05120bb2da6a1ee61fb33cd5c16732
helwyza <helwyza@gmail.com>
2024-10-03 01:07:50 +0700

    modif status in middleware

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -4,7 +4,7 @@ const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.token;
     if (!token) {
-      return res.status(403).json({
+      return res.status(401).json({
         message: "User not authenticated",
         success: false,
       });
