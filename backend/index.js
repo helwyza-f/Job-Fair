be5d6b1473266aa87f4fa058db306f0d5687bf6e
helwyza <helwyza@gmail.com>
2024-10-06 23:21:21 +0700

    edit root route

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   return res.status(200).json({
-    message: "Server is running",
+    message: `Server running at port ${PORT}`,
     success: true,
   });
 });
