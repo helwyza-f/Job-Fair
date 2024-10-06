35bda94094e62aac2dbe2d13ca70e68360aa2226
helwyza <helwyza@gmail.com>
2024-10-07 04:12:34 +0700

    edit axios

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -10,7 +10,7 @@ const useGetAllJobs = () => {
   useEffect(() => {
     const fetchAllJobs = async () => {
       try {
-        const res = await fetch(
+        const res = await axios.get(
           `${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,
           { withCredentials: true }
         );
