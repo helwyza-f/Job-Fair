d366bc601c9345b3355cb39b1cb11ff128232854
helwyza <helwyza@gmail.com>
2024-10-07 04:20:31 +0700

    trying to fix cors

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const corsOptions = {
-  origin: "https://job-fair-client.vercel.app/",
-  credentials: true,
-};
+app.use(
+  cors({
+    origin: "https://job-fair-client.vercel.app", // Ganti dengan URL front-end Anda
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Izinkan pengiriman cookie atau authorization header jika diperlukan
+  })
+);
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
